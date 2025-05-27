93a8535ce58c355712421e7d43c56a7b3adc7592
shariefbe <shariefbe@gmail.com>
2025-05-27 14:54:14 +0530

    Update [[...index]].tsx

--- pages/sign-in/[[...index]].tsx
+++ pages/sign-in/[[...index]].tsx
@@ -1,11 +1,16 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="bg-white shadow-md rounded-xl p-8 w-full max-w-md">
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" afterSignInUrl="/dashboard" />
+        <SignIn
+          path="/sign-in"
+          routing="path"
+          signUpUrl="/sign-up"
+          afterSignInUrl="/dashboard"
+        />
       </div>
     </div>
-  );
+  )
 }

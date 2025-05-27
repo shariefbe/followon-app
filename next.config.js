b2d68944e0bf92bcd5660f09ad353986c70da2cd
shariefbe <shariefbe@gmail.com>
2025-05-27 18:33:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+
+const nextConfig = {
+  images: {
+    domains: ['source.unsplash.com'], // ✅ Add your image domains here
+  }
+}
+
+module.exports = withNextra(nextConfig)

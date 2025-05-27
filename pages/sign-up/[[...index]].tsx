69cac7e33a49e71066830aadb6b5840c1d0b198c
shariefbe <shariefbe@gmail.com>
2025-05-27 19:19:42 +0530

    Update [[...index]].tsx

--- pages/sign-up/[[...index]].tsx
+++ pages/sign-up/[[...index]].tsx
@@ -8,7 +8,7 @@ export default function SignUpPage() {
           path="/sign-up"
           routing="path"
           signInUrl="/sign-in"
-          afterSignUpUrl="/app/dashboard"
+          afterSignUpUrl="/dashboard"
         />
       </div>
     </div>
